58964c98e87cdb94ac00c3c3a9519847714b2e4d
jimmy81726 <jimmy81726@gmail.com>
2025-03-04 16:05:26 +0800

    更新 vite.config.js 的 base 路徑並添加 gh-pages 依賴

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/react_w4hw/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/react_w7hw/" : "/",
   plugins: [react()],
 });
